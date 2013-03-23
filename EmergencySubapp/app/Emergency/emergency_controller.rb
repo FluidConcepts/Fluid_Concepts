d5b9c1b5a2091ee01fe6827a674871057b79c188
jcooke212 <jcooke212@gmail.com>
2013-03-23 16:59:56 -0400

    Change Time Storage
    Convert the RSS pubdate into a UNIX time-stamp before it is stored

--- EmergencySubapp/app/Emergency/emergency_controller.rb
+++ EmergencySubapp/app/Emergency/emergency_controller.rb
@@ -2,6 +2,7 @@ require 'rho/rhocontroller'
 require 'helpers/browser_helper'
 require 'helpers/emergency_helper'
 require 'rexml/document'
+require 'time'
 class EmergencyController < Rho::RhoController
   include BrowserHelper
   include REXML
@@ -59,7 +60,8 @@ class EmergencyController < Rho::RhoController
       desc = elm.elements["description"].text
       date_time = elm.elements["pubDate"].text
       date_array = [date_time[0..16], date_time[16..date_time.length-6]]
-      Emergency.create({ "title" => title, "description" => desc, "time" => date_array[1], "date" => date_array[0], "fullTime" => date_time})
+      nixTimeStamp = Time.parse(date_time).to_i
+      Emergency.create({ "title" => title, "description" => desc, "time" => date_array[1], "date" => date_array[0], "fullTime" => nixTimeStamp})
     end
     file.close
 	end
