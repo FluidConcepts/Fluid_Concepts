a39d3a5f1f93c7a5981f513a8351b62af28a438a
jcooke212 <jcooke212@gmail.com>
2013-03-23 18:39:05 -0400

    Define Home Download
    The download started by the Home model should act differently than a
    refresh call. Move the function into Home.

--- EmergencySubapp/app/Home/home_controller.rb
+++ EmergencySubapp/app/Home/home_controller.rb
@@ -30,9 +30,29 @@ class HomeController < Rho::RhoController
       :url => "https://php.radford.edu/~softeng02/rss-sim/rss.php",
       :filename => File.join(Rho::RhoApplication::get_base_app_path, "feed.xml"),
       :headers => {},
-      :callback => url_for(:controller => :Emergency, :action => :httpdownload_callback))
+      :callback => url_for(:action => :httpdownload_callback))
     Rho::Timer.start(4000, url_for(:action => :timer_callback), "test")
   end 
+  
+  # Do the download procedure for a non refresh call
+  def httpdownload_callback
+    Emergency.delete_all()
+    file = File.new(File.join(Rho::RhoApplication::get_base_app_path, "feed.xml"))
+    doc = REXML::Document.new(file)
+    firstLoop = true
+    #Parse each item element in this XML document.
+    doc.elements.each("*/channel/item")do |elm|
+      title = elm.elements["title"].text
+      desc = elm.elements["description"].text
+      date_time = elm.elements["pubDate"].text
+      date_array = [date_time[0..16], date_time[16..date_time.length-6]]
+      # We want the "fulltime" element in our database to be a UNIX time-stamp because comparisons are easier.
+      nixTimeStamp = Time.parse(date_time).to_i
+      # Create this Emergency object in the database.
+      Emergency.create({ "title" => title, "description" => desc, "time" => date_array[1], "date" => date_array[0], "fullTime" => nixTimeStamp})
+    end
+    file.close
+  end
   
   # Start a new thread that will sleep for 5 seconds to allow parsing to complete and then 
   # check the database for to see if there is a new alert. 
