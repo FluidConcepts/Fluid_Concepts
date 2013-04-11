b313181eb4d36d9a634f7aa32fb71340b484f531
jcooke212 <jcooke212@gmail.com>
2013-04-11 12:55:43 -0400

    Global Paths
    Make the file paths global variables in home_controller.rb

--- EmergencySubapp/app/Home/home_controller.rb
+++ EmergencySubapp/app/Home/home_controller.rb
@@ -5,7 +5,9 @@ require 'time'
 class HomeController < Rho::RhoController
   include BrowserHelper
   include REXML
-  
+  #####################################################
+  # !!MAY NEED TO CONFIGURE :url AROUND LINE 35!!     #
+  #####################################################
   # GET /Home
   def index
     @homes = Home.find(:all)
@@ -15,21 +17,23 @@ class HomeController < Rho::RhoController
   def checkNew
     # Open the file containing the information of the last update that was displayed and store 
     # it in value.
-    File.open(File.join(Rho::RhoApplication::get_base_app_path, "shown"), 
+    @shownPath = File.join(Rho::RhoApplication::get_base_app_path, "shown")
+    @feedPath = File.join(Rho::RhoApplication::get_base_app_path, "feed.xml")
+    File.open(@shownPath, 
       File::RDWR|File::CREAT){ |f|
         f.flock(File::LOCK_EX)
         @@value = f.read()
         f.close
     }  
     # Destroy feed.xml if it exists. We don't want our file to write improperly.
-    if File.exists?(File.join(Rho::RhoApplication::get_base_app_path, "feed.xml"))
-      File.delete(File.join(Rho::RhoApplication::get_base_app_path, "feed.xml"))
+    if File.exists?(@feedPath)
+      File.delete(@feedPath)
     end 
     # Download the new XML file representing the RSS feed. On download complete callback in 
     # emergency_controller will parse the XML and update the database.
     Rho::AsyncHttp.download_file(
       :url => "https://php.radford.edu/~softeng02/rss-sim/rss.php",
-      :filename => File.join(Rho::RhoApplication::get_base_app_path, "feed.xml"),
+      :filename => @feedPath,
       :headers => {},
       :callback => url_for(:action => :httpdownload_callback))
     Rho::Timer.start(4000, url_for(:action => :timer_callback), "test")
@@ -38,7 +42,7 @@ class HomeController < Rho::RhoController
   # Do the download procedure for a non refresh call
   def httpdownload_callback
     Emergency.delete_all()
-    file = File.new(File.join(Rho::RhoApplication::get_base_app_path, "feed.xml"))
+    file = File.new(@feedPath)
     doc = REXML::Document.new(file)
     firstLoop = true
     #Parse each item element in this XML document.
@@ -59,6 +63,7 @@ class HomeController < Rho::RhoController
   # Start a new thread that will sleep for 5 seconds to allow parsing to complete and then 
   # check the database for to see if there is a new alert. 
   def timer_callback
+    @shownPath = File.join(Rho::RhoApplication::get_base_app_path, "shown")
     @emergency = Emergency.find(:first)        # Get the newly update database
     if(@emergency != nil)                      # Can't display pop-up with no alerts
       @filter = @AppSettings.find(:first).NotificationTypes
@@ -73,10 +78,10 @@ class HomeController < Rho::RhoController
           {:id => 'Dismiss', :title => 'Dismiss'}],
           :callback => url_for(:controller => :Emergency, :action => :popup_handler) } )
         # Delete the file first. 
-        File.delete(File.join(Rho::RhoApplication::get_base_app_path, "shown"))
+        File.delete(@shownPath)
         # Write the full date and time of the shown pop-up to the last.txt file. We use this above
         # to filter seen pop-ups.    
-        File.open(File.join(Rho::RhoApplication::get_base_app_path, "shown"), 
+        File.open(@shownPath, 
           File::RDWR|File::CREAT){ |f|
             f.flock(File::LOCK_EX)
             f.write(@emergency.fullTime)      
